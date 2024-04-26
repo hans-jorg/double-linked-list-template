ed8826bc16438472f2cc92d417c38aa2f6fbef4c
Hans <hans.schneebeli@gmail.com>
2024-04-26 11:02:50 -0300

    Move implementation macro to template header

--- linked-list-template.h
+++ linked-list-template.h
@@ -64,5 +64,81 @@ void JOIN(LLT_PREFIX,InsertBefore)(LLT_NODE *node, LLT_NODE *givennode);        
 void JOIN(LLT_PREFIX,Delete)(LLT_NODE *node);
 
 
+#define INSERT_LINKED_LIST_IMPLEMENTATION(LLT_PREFIX,LLT_NODE)   \
+/**                                                              \
+ *  @brief  Insert node after given node                         \
+ */                                                              \
+int JOIN(LLT_PREFIX,Init)(LLT_NODE *node) {                      \
+                                                                 \
+    node->next = node;                                           \
+    node->prev = node;                                           \
+}                                                                \
+/**                                                              \
+ *  @brief  Insert node after given node                         \
+ */                                                              \
+LLT_NODE *JOIN(LIST_,Next)(LLT_NODE *node) {                     \
+                                                                 \
+    return node->next;                                           \
+}                                                                \
+                                                                 \
+/**                                                              \
+ *  @brief  Insert node after given node                         \
+ */                                                              \
+LLT_NODE *JOIN(LLT_PREFIX,Previous)(LLT_NODE *node) {            \
+                                                                 \
+    return node->prev;                                           \
+}                                                                \
+                                                                 \
+/**                                                              \
+ *  @brief  Insert node after given node                         \
+ */                                                              \
+void JOIN(LLT_PREFIX,InsertAfter)(LLT_NODE *node, LLT_NODE *givennode) {    \
+LLT_NODE *nextnode,*prevnode;                                    \
+                                                                 \
+    nextnode = givennode->next;                                  \
+    prevnode = nextnode->prev;                                   \
+                                                                 \
+    givennode->next = node;                                      \
+    nextnode->prev = node;                                       \
+                                                                 \
+    node->next = nextnode;                                       \
+    node->prev = givennode;                                      \
+                                                                 \
+}                                                                \
+                                                                 \
+/**                                                              \
+ *  @brief  Insert node after given node                         \
+ */                                                              \
+void JOIN(LLT_PREFIX,InsertBefore)(LLT_NODE *node, LLT_NODE *givennode) {   \
+LLT_NODE *nextnode,*prevnode;                                    \
+                                                                 \
+    prevnode = givennode->prev;                                  \
+    nextnode = prevnode->next;                                   \
+                                                                 \
+    node->next = givennode;                                      \
+    givennode->prev = node;                                      \
+                                                                 \
+    prevnode->next = node;                                       \
+    node->prev = prevnode;                                       \
+                                                                 \
+}                                                                \
+                                                                 \
+/**                                                              \
+ *  @brief  Remove a node from LIST_                             \
+ */                                                              \
+void JOIN(LLT_PREFIX,Delete)(LLT_NODE *node) {                   \
+LLT_NODE *prevnode,*nextnode;                                    \
+                                                                 \
+    prevnode = node->prev;                                       \
+    nextnode = node->next;                                       \
+                                                                 \
+    prevnode->next = nextnode;                                   \
+    nextnode->prev = prevnode;                                   \
+                                                                 \
+    /* To avoid dangling pointers */                             \
+    node->prev = 0;                                              \
+    node->next = 0;                                              \
+}                                                                \
+
 
 #endif // LINKED_LIST_TEMPLATE_H
